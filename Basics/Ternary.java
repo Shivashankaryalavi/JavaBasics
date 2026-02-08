507c12229c43afaf7bfa81177eddb642469d7248
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-02-08 12:43:49 +0530

    Add Java program for largest of three numbers using ternary operator

---
+++ Basics/Ternary.java
@@ -0,0 +1,21 @@
+package Basics;
+import java.util.Scanner;
+
+
+public class Ternary {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number1 :");
+        int num1 = sc.nextInt();
+        System.out.println("Enter a number2 :");
+        int num2 = sc.nextInt();
+        System.out.println("Enter a number3 :");
+        int num3 = sc.nextInt();
+
+        int largest = (num1>num2)? (num1>num3 ? num1: num3) : (num2>num3 ?num2 : num3);
+        System.out.println("The largest number is : " + largest);
+        sc.close();
+    }
+}
+
