0532e14190f8a247e47fe625adca4b77ee9c2771
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-02-10 18:29:43 +0530

    Java: lowercase conversion using object reference

---
+++ Basics/LowerCase.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class LowerCase {
+
+          String name ="SHIVASHANKAR";
+    public static void main(String[] args) {
+  
+       LowerCase l1 = new LowerCase();
+       System.out.println(l1.name.toLowerCase());
+       
+    }
+  
+}
+
