572a8245eb2144262670559e38522a3222ecaf1e
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-02-11 14:06:58 +0530

    Implement type casting example in Java

---
+++ Basics/TypeCasting.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        double d = 10.12;
+        int a = (int)d;
+        System.out.println(a);
+
+        //Also second way 
+        int c = (int)10.12;
+        System.out.println(c);
+    }
+    
+}
