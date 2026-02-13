682e9413a91ea47839961f6241d70d97ada4ba93
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-02-13 19:31:27 +0530

    Add Java program to check whether a number is positive, negative, or zero

---
+++ Operators/Positive.java
@@ -0,0 +1,23 @@
+package Operators;
+import java.util.*;
+
+public class Positive {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+
+        if(num < 0){
+            System.out.println(num + " is a negative number");
+        }
+        else if(num == 0){
+            System.out.println("The number is zero");
+        }
+        else{
+            System.out.println(num + " is a positive number");
+        }
+
+        sc.close();
+    }
+}
