cda5da7a57ec12a8774477a9edab80d3ed2facf2
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-02-19 20:10:31 +0530

    Added prime number program using optimized square root logic

---
+++ Loops/PrimeNumbers.java
@@ -0,0 +1,26 @@
+package Loops;
+import java.util.Scanner;
+public class PrimeNumbers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        System.out.println("Prime numbers upto " + n + ":");
+        for(int i =2;i<=n;i++){
+            boolean isPrime =true;
+            for(int j=2;j*j<=i;j++){
+                if(i%j==0){
+                    isPrime=false;
+                    break;
+                }
+
+            }
+            if(isPrime){
+                System.out.println(i + " ");
+            }
+          
+        }
+
+
+    }
+}
