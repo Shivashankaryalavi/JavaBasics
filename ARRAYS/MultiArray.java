4d4f84300703e0a8e6b055f38963907ce1de7c63
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-02-20 19:53:47 +0530

    Added dynamic 2D array input and matrix traversal

---
+++ ARRAYS/MultiArray.java
@@ -0,0 +1,34 @@
+package ARRAYS;
+
+import java.util.Scanner;
+
+public class MultiArray {
+
+    public static void main(String[] args) {
+              Scanner sc = new Scanner(System.in);
+              System.out.println("Enter a number");
+              int n= sc.nextInt();
+              
+              for(int i=1;i<=n;i++){
+                 boolean isPrime =true;
+                       if(i<=1)continue;
+           
+                 for(int j=2;j*j<=i;j++){
+                    if(i%j==0){
+                        isPrime=false;
+                        break;
+                    }
+                    
+                 }
+                 if(isPrime){
+                        System.out.println(i+" ");
+                    }
+                    
+
+
+              }
+      
+        }
+        
+        
+    }
---
+++ Basics/Laptops.java
@@ -0,0 +1,13 @@
+package Basics;
+import java.util.Scanner;
+public class Laptops {
+     static  int price =10000;
+     String brand ="HP";
+
+    public static void main(String[] args) {
+        System.out.println(price);
+        Laptops l1 = new Laptops();
+        System.out.println(l1.brand);
+        
+    }
+}
