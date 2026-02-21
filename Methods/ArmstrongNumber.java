5733fca9c3db07c5407e4aabe910286d24bfebd1
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-02-21 22:32:57 +0530

    Added Armstrong number program using methods

---
+++ Methods/ArmstrongNumber.java
@@ -0,0 +1,27 @@
+package Methods;
+import java.util.Scanner;
+public class ArmstrongNumber {
+    public static boolean  isArmstrong(int number){
+        int original=number;
+        int sum=0;
+        while(number>0){
+            int digit = number%10;
+            sum+=(digit*digit*digit);
+            number/=10;
+
+        }
+        return sum==original;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int number = sc.nextInt();
+        if(isArmstrong(number)){
+            System.out.println("Armstrong number");
+        }
+        else{
+            System.out.println("Not Armstrong number");
+        }
+        
+    }
+}
