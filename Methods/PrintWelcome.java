83d05308b6b0ddb48512858d662f7c7bd656ed10
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-02-21 19:59:34 +0530

    Added printWelcome method without parameters and return type

---
+++ Methods/PrintWelcome.java
@@ -0,0 +1,11 @@
+package Methods;
+public class PrintWelcome {
+    public static void printWelcome(){
+        System.out.println("Welcome");
+    }
+
+    public static void main(String[] args) {
+        printWelcome();
+        
+    }
+}
