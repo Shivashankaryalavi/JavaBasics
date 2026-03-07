132774c896f7f9a0d990ede140cfbdcc071603ca
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-03-07 12:20:48 +0530

    Remove lastdigit program

--- JavaProgramming/RemoveLastdigit.java
+++ JavaProgramming/RemoveLastdigit.java
@@ -1,15 +1,17 @@
 package JavaProgramming;
-import java.util.Scanner;
-public class Lsatdigit {
-    public static void lastdigitnum(int num){
-        int lastdigit = num%10;
-        System.out.println("Lastdigit of num is : " + lastdigit);
-    }
 
+import java.util.Scanner;
+
+public class RemoveLastdigit {
+
+    public static void removelastdigit(int num){
+        int removelastdigit = num/10;
+        System.out.println(removelastdigit);
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number : ");
-        lastdigitnum(sc.nextInt());
+        removelastdigit(sc.nextInt());
         sc.close();
     }
 }
