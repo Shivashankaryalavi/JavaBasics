c92c3a3b7bdc75785f94eebc668bd4dda6004528
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-03-20 22:34:00 +0530

    Added recursive factorial program with user input

---
+++ Basics/PrintNumbers.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class PrintNumbers {
+
+    public static void printNumbers(int n){
+        if(n>10){
+            return ;
+        }
+        System.out.println(n +" ");
+
+        printNumbers(n+1); 
+
+        
+
+    }
+    public static void main(String[] args) {
+        printNumbers(1);
+    }
+}
