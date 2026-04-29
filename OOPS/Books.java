294592b016f12aafd1805c9cc0a58e6da254d96a
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-04-29 20:06:40 +0530

    Add java program of whether is sorted or not

---
+++ OOPS/Books.java
@@ -0,0 +1,22 @@
+package OOPS;
+
+
+class Book{
+    double price;
+    String name;
+    Book(double price,String name){
+        this.price=price;
+        this.name=name;
+    }
+
+}
+
+public class Books {
+    public static void main(String[] args) {
+    6Book b1 = new Book(9000.0, "java");
+    System.out.println(b1);
+        
+    }
+
+    
+}
