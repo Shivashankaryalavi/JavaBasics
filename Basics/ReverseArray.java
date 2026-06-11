76845c82dc8e2fd103f3f28d39862f64ef84e7e5
Shivashankaryalavi <shivashankaryalavi@gmail.com>
2026-06-11 19:09:24 +0530

    Add java program to reverse order

---
+++ Basics/ReverseArray.java
@@ -0,0 +1,18 @@
+package Basics;
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        int temp[] = new int[arr.length];
+        int j=0;
+        for(int i=arr.length-1;i>=0;i--){
+            temp[j] = arr[i];
+            j++;
+        }
+     System.out.println(Arrays.toString(temp));
+    }
+    
+}
